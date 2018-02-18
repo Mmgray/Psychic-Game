965ced64a9d8b52669a1581b23ef768a6e0ae9e2
Misti Gray <misti.mechelle@gmail.com>
2018-02-17 23:35:36 -0600

    game updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,38 +3,31 @@ var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L
 var wins = 0;
 var losses = 0;
 var numGuesses = 10;
-var lettersGuessed = [];
+var lettersGuessed =[];
 
-document.onkeyup = function() {
-    var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
-    // userGuess = computerChoices[];
-    //String of letters guessed
-       lettersGuessed = userGuess;
-
+document.onkeyup = function(event) {
+    var userGuess = event.key.toUpperCase();
+    lettersGuessed.push(userGuess);
     //Computer's random guess
     var computerGuess = computerChoices[Math.floor (Math.random() * computerChoices.length)];
     
     //How user adds to the win
     if (userGuess === computerGuess) {
-        wins++;
+        win++;
     } 
     //How user losses 
-     else {
-         losses++;
+     else  {
          numGuesses--;
      }
-     //Game over
-     if (numGuesses < 0){
-         //reset
-         wins=0;
-         losses=0
+
+     if (numGuesses === 0){
+        
+         losses++;
          numGuesses=10;
          lettersGuessed=[];
-         alert("Game Over!")
      }
      var html = "<h1>The Psychic Game</h1>" + "<br>"+"<h2>Guess what letter I'm thinking of</h2>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + numGuesses + "</p>" + "<p>Your Guess So far: " + lettersGuessed + "</p>";
 
      document.querySelector("#game").innerHTML = html;
-   
      
     };
